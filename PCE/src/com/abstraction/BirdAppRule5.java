3e970ee15c484e55a195656ba8047507fc8b4b2c
anuragchavhan0 <anuragchavhan0@localhost>
2026-01-01 16:38:14 +0530

    Rules of inheritance

--- PCE/src/com/abstraction/BirdAppRule5.java
+++ PCE/src/com/abstraction/BirdAppRule5.java
@@ -1,10 +1,34 @@
 package com.abstraction;
 
-public class BirdAppRule5 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+abstract public class BirdAppRule5{
+	
+	public static void main(String []args) {
+		System.out.println(a);
+		disp();
+		
 	}
+	
+	
+	
+	public BirdAppRule5() {
+		
+	}
+	
+static int a;
+static {
+	a=10;
+}
+static void disp() {
+	System.out.println(a);
+}
 
+
+ int b;
+ {
+	b=10;
+}
+ void disp2() {
+	System.out.println(b);
+}
+ 
 }
--- PCE/src/com/abstraction/BirdRule6.java
+++ PCE/src/com/abstraction/BirdRule6.java
@@ -1,5 +1,13 @@
-package com.abstraction;
+//package com.abstraction;
 
-public class BirdRule6 {
 
-}
+//abstract class bird{ //pure abstract class 
+//	abstract void eat();
+//	 abstract void fly();	
+	// }
+
+//abstract  class sparrow4 extends bird{ //unpure abstract days 
+//	void  fly() {
+//		System.out.println("flying ");
+	//}
+//}
